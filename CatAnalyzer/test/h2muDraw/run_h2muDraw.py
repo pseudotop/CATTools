2c4e1b0b59a60402ed2d8abff4fec3580c69d4c4
pseudotop <lsupertopl@gmail.com>
2015-12-03 18:22:07 +0900

    updated h2muDraw package

--- CatAnalyzer/test/h2muDraw/run_h2muDraw.py
+++ CatAnalyzer/test/h2muDraw/run_h2muDraw.py
@@ -3,19 +3,31 @@
 import ROOT,os,sys,copy,json
 ROOT.gROOT.SetBatch(True)
 
-execmd = "h2muDraw.py"
 
 cmssw = os.environ['CMSSW_BASE']
+
 outDir = "plot"
+exe = "h2muDraw.py"
 if not os.path.isdir(outDir):os.mkdir(outDir)
+abspathOut = os.path.abspath("./")
+abspathExe = "%s/%s"%(abspathOut,exe)
+
 try:
     info_json = json.load(open("%s/info.json" % outDir))
-except:"""
-!! WARNNING
+    isdatajson = os.path.isfile("%s/src/CATTools/CatAnalyzer/data/dataset.json" % os.environ['CMSSW_BASE'])
+except:
+    info_json = None
+    isdatajson = None
 
-   please try to run "set_config.py" file which makes json file at first.
-   and then you can run this file.
-"""
+if (info_json==None) or (isdatajson==None):
+    print """
+    !! WARNNING !!
+
+    1. before you run, dataset.json file in "data" folder is required
+    2. please try to run "set_config.py" file which makes json file at first.
+    and then you can run this file.
+    """
+    sys.exit(2)
 
 sh_s = """#!/bin/bash
 workD=%s
@@ -23,9 +35,7 @@ source /cvmfs/cms.cern.ch/cmsset_default.sh
 cd $workD
 eval `scramv1 runtime -sh`
 cd -
-mkdir -p %s
-host=`hostname`
-./%s
+%s
 """
 
 jds_s = """
@@ -33,18 +43,18 @@ executable = %s
 universe = vanilla
 getenv = True
 
+dir= %s/plot
 num= %s
-dirSaved= %s
 fname= %s
 
-error = $(dirSaved)/error_$(num)
-log = $(dirSaved)/log_$(num)
+error = $(dir)/error_$(num)
+log = $(dir)/log_$(num)
 
 should_transfer_files = YES
 when_to_transfer_output = ON_EXIT
-transfer_input_files = h2muDraw.py
+transfer_input_files = %s
 transfer_output_files = $(fname).png
-transfer_output_remaps = "$(fname).png = $(dirSaved)/$(fname).png"
+transfer_output_remaps = "$(fname).png = $(dir)/$(fname).png"
 
 queue
 """ 
@@ -55,20 +65,21 @@ for i,info in enumerate(info_json):
     else:
         args = "-c \'%s\' -b %s -p %s -x \'%s\' -y \'%s\' -f \'%s\'"%(cut,binning,plotvar,x_name,y_name,f_name)
     
-    #if not i<1:continue
+    if not i<1:continue
     name_sh, name_jds = "%s/tmp_%s.sh"%(outDir,i),"%s/tmp_%s.jds"%(outDir,i)
-    f_sh = sh_s%(cmssw, outDir,execmd+" "+args)
+    f_sh = sh_s%(cmssw,abspathExe+" "+args)
     tmp_f_sh = open(name_sh,"w")
     tmp_f_sh.write(f_sh)
     tmp_f_sh.close()
-    f_jds = jds_s%(name_sh,i,"%s"%(outDir),f_name)
+    f_jds = jds_s%("tmp_%s.sh"%i,abspathOut,i,f_name,abspathExe)
     tmp_f_jds = open(name_jds,"w")
     tmp_f_jds.write(f_jds)
     tmp_f_jds.close()
     os.system("chmod 755 %s"%name_jds)
     os.system("chmod 755 %s"%name_sh)
-    os.system("condor_submit %s"%name_jds)
+    os.system("condor_submit %s"%(name_jds))
     #os.system("rm -f tmp.jds")
     #os.system("rm -f tmp.sh")
     print "%d/%d"%(i,len(info_json))
+    #os.system("mv -t %s %s %s"%(outDir,name_sh,name_jds))
 #print ("./h2muDraw.py -c \"%s\" -b %s -p %s -x %s -y %s -f %s/%s&"%(cut,binning,plotvar,x_name,y_name,outDir,f_name))
