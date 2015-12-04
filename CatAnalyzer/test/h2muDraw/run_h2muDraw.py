fe75abb0b0a46117254dd283235342f94ee0f2c6
pseudotop <lsupertopl@gmail.com>
2015-12-05 00:29:01 +0900

    fixed h2muDraw condor_submit job

--- CatAnalyzer/test/h2muDraw/run_h2muDraw.py
+++ CatAnalyzer/test/h2muDraw/run_h2muDraw.py
@@ -9,6 +9,9 @@ cmssw = os.environ['CMSSW_BASE']
 outDir = "plot"
 exe = "h2muDraw.py"
 if not os.path.isdir(outDir):os.mkdir(outDir)
+print os.environ['HOSTNAME']
+if os.environ['HOSTNAME'].endswith('sdfarm.kr'):
+    os.chmod(outDir,0755)
 abspathOut = os.path.abspath("./")
 abspathExe = "%s/%s"%(abspathOut,exe)
 
@@ -30,31 +33,26 @@ if (info_json==None) or (isdatajson==None):
     sys.exit(2)
 
 sh_s = """#!/bin/bash
-workD=%s
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-cd $workD
-eval `scramv1 runtime -sh`
-cd -
-%s
+./%s
 """
 
-jds_s = """
-executable = %s
+jds_s = """executable = %s
 universe = vanilla
 getenv = True
 
-dir= %s/plot
+abspath= %s
 num= %s
 fname= %s
 
-error = $(dir)/error_$(num)
-log = $(dir)/log_$(num)
+error = error_$(num)
+log = log_$(num)
 
 should_transfer_files = YES
 when_to_transfer_output = ON_EXIT
-transfer_input_files = %s
+transfer_input_files = ./h2muDraw.py
 transfer_output_files = $(fname).png
-transfer_output_remaps = "$(fname).png = $(dir)/$(fname).png"
+transfer_output_remaps = "$(fname).png = $(abspath)/plot/$(fname).png"
 
 queue
 """ 
@@ -65,18 +63,19 @@ for i,info in enumerate(info_json):
     else:
         args = "-c \'%s\' -b %s -p %s -x \'%s\' -y \'%s\' -f \'%s\'"%(cut,binning,plotvar,x_name,y_name,f_name)
     
-    if not i<1:continue
-    name_sh, name_jds = "%s/tmp_%s.sh"%(outDir,i),"%s/tmp_%s.jds"%(outDir,i)
-    f_sh = sh_s%(cmssw,abspathExe+" "+args)
+    if not i<5:continue
+    #name_sh, name_jds = "%s/tmp_%s.sh"%(outDir,i),"%s/tmp_%s.jds"%(outDir,i)
+    name_sh, name_jds = "tmp_%s.sh"%i,"tmp_%s.jds"%i
+    f_sh = sh_s%(exe+" "+args)
     tmp_f_sh = open(name_sh,"w")
     tmp_f_sh.write(f_sh)
     tmp_f_sh.close()
-    f_jds = jds_s%("tmp_%s.sh"%i,abspathOut,i,f_name,abspathExe)
+    f_jds = jds_s%(name_sh,abspathOut,i,f_name)
     tmp_f_jds = open(name_jds,"w")
     tmp_f_jds.write(f_jds)
     tmp_f_jds.close()
-    os.system("chmod 755 %s"%name_jds)
-    os.system("chmod 755 %s"%name_sh)
+    os.chmod(name_jds,0755)
+    os.chmod(name_sh,0755)
     os.system("condor_submit %s"%(name_jds))
     #os.system("rm -f tmp.jds")
     #os.system("rm -f tmp.sh")
